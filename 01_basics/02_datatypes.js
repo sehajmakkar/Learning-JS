75d2c339926a16483aeb3fb9f53d550b6a929069
Sehaj Makkar <143745246+sehajmakkar@users.noreply.github.com>
2024-03-14 15:16:25 +0000

    datatypes done!

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+// all the code in the file will be considered as newer version of js code
+"use strict";
+
+// this will throw error as we are running JS over node not on HTML or web browser console
+// alert(3+3)
+
+// primitive data-types
+let name = "Sehaj" //string -> prefer to use in "_"
+let age = 18 // number
+let isLoggedIn = false // bool -> true or false
+let temp = null // null datatype -> stand alone value -> when asked the type of a null variable: it returns OBJECT but why?
+let state // undefined -> if you just decclare JS will say it is undefined 
+// symbol (implementation in React to find uniqueness of a content)
+
+// Null is an object but undefined is a type
+console.log(typeof(undefined)); //undefined
+console.log(typeof null); //object
