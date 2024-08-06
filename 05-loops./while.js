4d6d618386ef142713ab9dcbe7001459d797263c
SEHAJ <143745246+sehajmakkar@users.noreply.github.com>
2024-08-06 19:34:00 +0000

    while and do while

---
+++ 05-loops./while.js
@@ -0,0 +1,19 @@
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i+=2;
+// }
+
+// in array
+let arr = 0
+let myArray = [1,2,3,4,5,0,9,8,7]
+while(arr < myArray.length){
+    console.log(myArray[arr]);
+    arr++;
+}
+
+// 1 baar toh chalega hi chalega
+let score = 11;
+do {
+    console.log(score)
+} while (score <= 10);
